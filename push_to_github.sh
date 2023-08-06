76ff2bcce9876c9be538d7271754fb8e410fefc3
Mack Merriman <merriman@princeton.edu>
2023-08-06 16:51:13 -0700

    Daily game

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+while ! ping -c 1 -W 1 8.8.8.8; do
+    sleep 1
+done
 
 cd PuckPuzzle
 . .PPvenv/bin/activate
@@ -6,3 +9,4 @@ python manage.py create_daily_game
 git add .
 git commit -m "Daily game"
 git push origin main
+osascript -e 'display notification "PuckPuzzle was reset!" with title "PuckPuzzle"'
